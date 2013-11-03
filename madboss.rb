73f0579984c2210276b17591c5ce64f486051ea9
curtisaallen <curtisaallen@gmail.com>
2013-11-03 18:16:21 -0500

    updated the name.rb and madboss.rb file

--- madboss.rb
+++ madboss.rb
@@ -1,3 +1,3 @@
-puts "WHAT DO YOU WANT?"
+puts "WHAT DO YOU\'S WANT?"
 answer = gets.chomp
 puts 'WHADDAYA MEAN "' + answer.upcase + '"?!? YOURE FIRED!!'
--- name.rb
+++ name.rb
@@ -1,4 +1,6 @@
 puts 'Hello there, and whats your name?'
-name = gets
-puts 'Your name is  '+ name + '  ? what a lovely name!'
+name = gets.chomp
+puts 'Your name is '+ name + '? what a lovely name!'
+if name == 'curtis'
 puts 'Pleased to meet you, ' + name + '. :)'
+end
