ccb87479164e19fefd57b96defde3403816c0891
curtisaallen <curtisaallen@gmail.com>
2013-11-12 23:15:21 -0500

    add ruby 99 bottles of beer app

---
+++ 99Bottlesofbeer.rb
@@ -0,0 +1,6 @@
+beer = 99
+while beer != 0
+ puts("#{beer} bottles of beer on the wall, #{beer} bottles of beer."  )
+ beer -=1
+ puts("Take one down and pass it around, #{beer} bottles of beer on the wall."  )
+end
