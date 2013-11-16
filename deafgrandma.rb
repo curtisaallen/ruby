6ea0628f3d39d1357b1aa9d3c1e017241e3c5c88
curtisaallen <curtisaallen@gmail.com>
2013-11-16 09:18:12 -0500

    extended deaf grandma program

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,8 +1,13 @@
+puts "What would you like to ask Deaf Grandma?"
+val = nil
 dates = ["1930","1940","1950","1938"]
-while true
-	puts "What would you like to ask Deaf Grandma?"
-	val = gets.chomp
-	if val == val.upcase
+bye = 0
+while bye < 3
+    val = gets.chomp
+	if val == "BYE"
+	  puts "Hmmm.. I would prefer"
+	  bye = (bye+1)
+	elsif val == val.upcase
 		puts "No, NOT SINCE " + dates.sample + "!"
 		break
 	else
