6a160e4e4ff6b0831428863d80cf644367addddb
curtisaallen <curtisaallen@gmail.com>
2013-11-15 23:54:04 -0500

    updated deafgrandma program

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,8 +1,9 @@
+dates = ["1930","1940","1950","1938"]
 while true
 	puts "What would you like to ask Deaf Grandma?"
 	val = gets.chomp
 	if val == val.upcase
-		puts "No, NOT SINCE 1938!"
+		puts "No, NOT SINCE " + dates.sample + "!"
 		break
 	else
 		puts "HUH?! SPEAK UP, SONNY!"
